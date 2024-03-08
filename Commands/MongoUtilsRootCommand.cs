698fdb7ddbd981eb45da42652553327281d01fab
Mike Rowley <mike.rowley@gmail.com>
2024-03-08 14:52:13 -0700

    Created Root Command

---
+++ Commands/MongoUtilsRootCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.CommandLine;
+
+namespace BlueGoat.MongoDBUtils.Commands
+{
+    public class MongoUtilsRootCommand : RootCommand
+    {
+        public MongoUtilsRootCommand() : base("MongoDB Utilities")
+        {
+            Name = "mongo-utils";
+
+            AddGlobalOption(MongoUtilOptions.Connection);
+            AddGlobalOption(new Option<bool>("--debug") { IsHidden = true });
+            TreatUnmatchedTokensAsErrors = true;
+
+            Add(new MigrationCommand());
+            Add(new DropDatabaseCommand());
+            Add(new ResetDatabaseCommand());
+            Add(new HealthCheckCommand());
+            Add(new ScenarioCommand());
+        }
+
+        public sealed override string Name
+        {
+            get => base.Name;
+            set => base.Name = value;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,40 +6,19 @@ namespace BlueGoat.MongoDBUtils;
 
 public class Program
 {
-    static async Task<int> Main(params string[] args)
+    public static async Task<int> Main(params string[] args)
     {
         if (args.Any(x => x.Contains("--debug")))
         {
             Debugger.Launch();
         }
 
-        var rootCommand = new RootCommand("MongoDB Utilities")
-        {
-            new MigrationCommand(),
-            new DropDatabaseCommand(),
-            new ResetDatabaseCommand(),
-            new HealthCheckCommand(),
-            new ScenarioCommand()
-        };
-        rootCommand.Name = "mongo-utils";
-        rootCommand.AddGlobalOption(MongoUtilOptions.Connection);
-        rootCommand.AddGlobalOption(new Option<bool>("--debug") { IsHidden = true });
-
-        rootCommand.TreatUnmatchedTokensAsErrors = true;
+        var rootCommand = new MongoUtilsRootCommand();
 
         try
         {
             var result = await rootCommand.InvokeAsync(args);
             return result;
-        }
-        catch (CommandException ex)
-        {
-            ConsoleEx.WriteError(ex.Message);
-            return 0;
-        }
-        catch (Exception ex)
-        {
-            ConsoleEx.WriteError("Error executing command: " + ex.Message);
         }
         finally
         {
