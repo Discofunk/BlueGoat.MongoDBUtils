951b5866885b1c8983e70346d1fa284789f8b57a
Mike Rowley <mike.rowley@gmail.com>
2024-03-11 21:02:35 -0600

    Initial context and MongoDB TestContainers setup

---
+++ BlueGoat.MongoUtils.Test.Integration/DeleteMe.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using Xunit;
+
+namespace BlueGoat.MongoUtils.Test.Integration
+{
+    [Collection(MongoDbCollection.Name)]
+    public class DeleteMe
+    {
+        private readonly MongoDbTestContext mongoDbTestContext;
+
+        public DeleteMe(MongoDbTestContext mongoDbTestContext)
+        {
+            this.mongoDbTestContext = mongoDbTestContext;
+        }
+
+        [Fact]
+        public async Task CanConnect()
+        {
+            using var databases = await mongoDbTestContext.Client.ListDatabasesAsync();
+            Assert.True(await databases.AnyAsync());
+        }
+    }
+}
