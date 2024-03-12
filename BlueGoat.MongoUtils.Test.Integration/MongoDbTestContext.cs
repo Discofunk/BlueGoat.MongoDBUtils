fecd58574249c145144b324c7e67d19319d3c74d
Mike Rowley <mike.rowley@gmail.com>
2024-03-11 21:02:35 -0600

    Initial context and MongoDB TestContainers setup

---
+++ BlueGoat.MongoUtils.Test.Integration/MongoDbTestContext.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Driver;
+using Testcontainers.MongoDb;
+using Xunit;
+
+namespace BlueGoat.MongoUtils.Test.Integration
+{
+    public class MongoDbTestContext : IAsyncLifetime
+    {
+        private readonly MongoDbContainer mongoDbContainer =
+            new MongoDbBuilder().Build();
+
+        public string ConnectionString => mongoDbContainer.GetConnectionString();
+        public IMongoClient Client => new MongoClient(ConnectionString);
+
+        public Task InitializeAsync() => mongoDbContainer.StartAsync();
+
+        public Task DisposeAsync() => mongoDbContainer.DisposeAsync().AsTask();
+    }
+
+    [CollectionDefinition(Name)]
+    public class MongoDbCollection : ICollectionFixture<MongoDbTestContext>
+    {
+        public const string Name = "MongoDB Collection";
+    }
+}
